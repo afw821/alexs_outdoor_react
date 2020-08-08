bcfee647ef03a1e60ef4caa2efdeefadf60efb1a
afw821 <afw821@gmail.com>
2020-08-08 12:17:25 -0400

    re-connectiong back to the jawsDB URL for now

--- models/index.js
+++ models/index.js
@@ -11,14 +11,10 @@ var db = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  //var sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-  //for prod with new db
   var sequelize = new Sequelize(
     config.database,
-    config.user,
+    config.username,
     config.password,
-    config.host,
     config
   );
 }
