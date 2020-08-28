31074cf9669f820fbcb05e6dc73e9cc725e7e26a
afw821 <afw821@gmail.com>
2020-08-27 20:34:46 -0400

    Changed the api urls for deployment

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,7 +5,7 @@ import { apiUrl, deployedApiUrl } from "../config.json";
 http.setJwt(getJwt());
 
 export async function login(email, password) {
-  const { data: jwt } = await http.post(apiUrl + "/auth", {
+  const { data: jwt } = await http.post(deployedApiUrl + "/auth", {
     email,
     password,
   });
@@ -18,7 +18,7 @@ export function updatePassword(email, oldPassword, newPassword) {
     oldPassword: oldPassword,
     newPassword: newPassword,
   };
-  const result = http.put(apiUrl + "/auth/updatePassword", reqBody);
+  const result = http.put(deployedApiUrl + "/auth/updatePassword", reqBody);
 
   return result;
 }
@@ -46,7 +46,7 @@ export function getJwt() {
 }
 
 export async function regenerateToken(user) {
-  const token = await http.post(apiUrl + "/auth/generateToken/", user);
+  const token = await http.post(deployedApiUrl + "/auth/generateToken/", user);
 
   return token;
 }
--- client/src/services/categoryService.js
+++ client/src/services/categoryService.js
@@ -2,18 +2,18 @@ import http from "./httpService";
 import { apiUrl, deployedApiUrl } from "../config.json";
 
 export function getCategories() {
-  return http.get(apiUrl + "/categories");
+  return http.get(deployedApiUrl + "/categories");
 }
 
 export function addCategory(category) {
   const obj = {
     name: category,
   };
-  return http.post(apiUrl + "/categories", obj);
+  return http.post(deployedApiUrl + "/categories", obj);
 }
 
 export function getCategoryByPKId(categoryId) {
-  return http.get(apiUrl + `/categories/byPK/${categoryId}`);
+  return http.get(deployedApiUrl + `/categories/byPK/${categoryId}`);
 }
 
 export function deleteCategory(categoryId) {}
