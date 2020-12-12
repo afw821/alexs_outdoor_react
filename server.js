493fe9bba0c5f13e422d9ded9b9f9de2f5d120ae
afw821 <afw821@gmail.com>
2020-12-12 13:46:41 -0500

    removed force: true statement from db sync in server.js for re-deployment

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("*", (req, res) => {
 
 //server
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
   const PORT = process.env.PORT || 3922;
   app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
 });
